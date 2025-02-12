d0747ff8de180b9b581f502deebc287a8515ce13
Serhii <0992830717s@gmail.com>
2025-02-12 20:35:05 +0100

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿using System_311.Treading;
+﻿using System_311.Threading;
+using System_311.Tasks;
 
 new ThreadingDemo().Run();
+Console.WriteLine("______________________________________________________");
+//new TasksDemo().Run();
+
+Console.WriteLine("Main finished");

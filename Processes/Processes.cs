84ccdcf32ffe5838650c808431bde807b2773288
Serhii <0992830717s@gmail.com>
2025-02-15 07:36:44 +0100

    345

---
+++ Processes/Processes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+
+namespace System_311.Processes
+{
+    internal class Processes
+    {
+        public void Run()
+        {
+            String? choice;
+            do
+            {
+                Console.WriteLine("Which processes to stert?");
+                Console.WriteLine("1 - Notepad");
+                Console.WriteLine("2 - Notepad with Program.cs");
+                Console.WriteLine("0 - Exit");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1": Process.Start("notepad.exe"); break;
+                    case "2": 
+                        String dir=Directory.GetCurrentDirectory();
+                        dir=Regex.Replace(dir, @"\\bin\\.*", @"\");
+                        Process.Start("notepad.exe",Path.Combine(dir,"Program.cs"));
+                        Console.WriteLine(dir); 
+                        break;
+                }
+            }
+            while (choice !="0");
+            
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using System_311.Threading;
 using System_311.Tasks;
+using System_311.Processes;
 
-new ThreadingDemo().Run();
-Console.WriteLine("______________________________________________________");
-new TasksDemo().Run();
-
+//new ThreadingDemo().Run();
+//Console.WriteLine("______________________________________________________");
+//new TasksDemo().Run();
+new Processes().Run();
